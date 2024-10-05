f449150e2618a9c59a936794af45d2d5dd0238f7
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2024-10-05 12:30:14 +0930

    DEV: Add macOS specific functionality for app handling and window customization - updated license to AGPL v3.0

---
+++ src-tauri/src/app_handle.rs
@@ -0,0 +1,46 @@
+// from https://github.com/overlayeddev/overlayed/blob/2b1e7da70ae2f514fe067c11e954403288dd5f32/apps/desktop/src-tauri/src/app_handle.rs
+#[cfg(target_os = "macos")]
+use tauri::{AppHandle, Runtime};
+
+#[cfg(target_os = "macos")]
+use tauri_nspanel::{
+    cocoa::base::id,
+    objc::{class, msg_send, sel, sel_impl},
+};
+
+#[cfg(target_os = "macos")]
+fn nsstring_to_string(ns_string: id) -> Option<String> {
+    use std::ffi::{c_char, CStr};
+
+    let utf8: id = unsafe { msg_send![ns_string, UTF8String] };
+
+    if !utf8.is_null() {
+        Some(unsafe {
+            {
+                CStr::from_ptr(utf8 as *const c_char)
+                    .to_string_lossy()
+                    .into_owned()
+            }
+        })
+    } else {
+        None
+    }
+}
+
+#[cfg(target_os = "macos")]
+pub trait AppHandleExt {
+    fn frontmost_application_bundle_id() -> Option<String>;
+}
+
+#[cfg(target_os = "macos")]
+impl<R: Runtime> AppHandleExt for AppHandle<R> {
+    fn frontmost_application_bundle_id() -> Option<String> {
+        let workspace: id = unsafe { msg_send![class!(NSWorkspace), sharedWorkspace] };
+
+        let frontmost_application: id = unsafe { msg_send![workspace, frontmostApplication] };
+
+        let bundle_id: id = unsafe { msg_send![frontmost_application, bundleIdentifier] };
+
+        nsstring_to_string(bundle_id)
+    }
+}
---
+++ src-tauri/src/window_custom.rs
@@ -0,0 +1,59 @@
+// from https://github.com/overlayeddev/overlayed/blob/2b1e7da70ae2f514fe067c11e954403288dd5f32/apps/desktop/src-tauri/src/window_custom.rs
+#[cfg(target_os = "macos")]
+pub mod macos {
+    use cocoa::appkit::{NSWindow, NSWindowCollectionBehavior};
+    use tauri::{Runtime, WebviewWindow};
+    use tauri_nspanel::WebviewWindowExt;
+
+    pub trait WebviewWindowExtMacos {
+        fn remove_shadow(&self);
+
+        fn set_float_panel(&self, level: i32);
+    }
+
+    impl<R: Runtime> WebviewWindowExtMacos for WebviewWindow<R> {
+        fn remove_shadow(&self) {
+            unsafe {
+                let id = self.ns_window().unwrap() as cocoa::base::id;
+
+                #[cfg(target_arch = "aarch64")]
+                id.setHasShadow_(false);
+
+                #[cfg(target_arch = "x86_64")]
+                id.setHasShadow_(0);
+            }
+        }
+
+        fn set_float_panel(&self, level: i32) {
+            let panel = self.to_panel().unwrap();
+
+            panel.set_level(level);
+
+            #[allow(non_upper_case_globals)]
+            const NSWindowStyleMaskNonActivatingPanel: i32 = 1 << 7;
+
+            #[allow(non_upper_case_globals)]
+            const NSResizableWindowMask: i32 = 1 << 3;
+
+            panel.set_style_mask(NSWindowStyleMaskNonActivatingPanel + NSResizableWindowMask);
+
+            panel.set_collection_behaviour(
+                NSWindowCollectionBehavior::NSWindowCollectionBehaviorCanJoinAllSpaces
+                    | NSWindowCollectionBehavior::NSWindowCollectionBehaviorFullScreenAuxiliary,
+            );
+        }
+    }
+}
+
+use tauri::{Runtime, WebviewWindow};
+
+pub trait WebviewWindowExt {
+    fn set_document_title(&self, url: &str);
+}
+
+impl<R: Runtime> WebviewWindowExt for WebviewWindow<R> {
+    fn set_document_title(&self, title: &str) {
+        let str = format!("document.title = '{:}'", title);
+        self.eval(&str).unwrap();
+    }
+}
