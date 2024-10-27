74c169e68341151022bf216dba600c2b45a2d865
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2024-10-27 11:37:02 +1030

    FIX: Refactor components to reflect new architecture

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,68 +1,59 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy } from "@angular/core";
 import { MatRippleModule } from "@angular/material/core";
 import { MatIconModule } from "@angular/material/icon";
+import { Color, colors, ThemeService } from "../../services/theme.service";
+import { MatSelectModule } from "@angular/material/select";
 import {
   ColorPreference,
   LightingPreference,
-  ThemeService,
-} from "../../services/theme.service";
-import { invoke } from "@tauri-apps/api/core";
-import { SettingsService } from "../../services/settings.service";
-
-interface ColorDuo {
-  from: string;
-  to: string;
-}
-
-interface Color {
-  name: ColorPreference;
-  light: ColorDuo;
-  dark: ColorDuo;
-}
+  Settings,
+  SettingsService,
+} from "../../services/settings.service";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-settings-page",
   standalone: true,
-  imports: [MatIconModule, MatRippleModule],
+  imports: [
+    MatIconModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+  ],
   templateUrl: "./settings-page.component.html",
   styleUrl: "./settings-page.component.scss",
 })
-export class SettingsPageComponent {
-  colors: Color[] = [
-    {
-      name: ColorPreference.DEFAULT,
-      light: { from: "from-gray-400", to: "to-gray-500" },
-      dark: { from: "from-gray-700", to: "to-gray-800" },
-    },
-    {
-      name: ColorPreference.AZURE,
-      light: { from: "from-blue-400", to: "to-blue-500" },
-      dark: { from: "from-blue-700", to: "to-blue-800" },
-    },
-    {
-      name: ColorPreference.YELLOW,
-      light: { from: "from-yellow-400", to: "to-yellow-500" },
-      dark: { from: "from-yellow-700", to: "to-yellow-800" },
-    },
-    {
-      name: ColorPreference.CYAN,
-      light: { from: "from-cyan-400", to: "to-cyan-500" },
-      dark: { from: "from-cyan-700", to: "to-cyan-800" },
-    },
-  ];
-  selectedLighting = "system";
+export class SettingsPageComponent implements OnDestroy {
+  colors: Color[] = colors;
+  settingsSubscription: Subscription;
+  settings?: Settings;
 
-  constructor(protected ts: ThemeService, private ss: SettingsService) {}
+  constructor(protected ts: ThemeService, private ss: SettingsService) {
+    this.settingsSubscription = this.ss.settings$.subscribe((settings) => {
+      this.settings = settings;
+    });
+  }
 
-  async changeColor(theme: Color) {
-    const newTheme = this.ts.changeColor(theme.name);
-    const settings = await this.ss.get();
-    this.ss.update({ ...settings, ...newTheme });
+  async changeColor(color: ColorPreference) {
+    const settings = this.settings;
+    if (!settings) return;
+    this.ss.update({ ...settings, color: color });
   }
 
   async changeLighting(lighting: LightingPreference) {
-    const newTheme = this.ts.changeLighting(lighting);
-    const settings = await this.ss.get();
-    this.ss.update({ ...settings, ...newTheme });
+    const settings = this.settings;
+    if (!settings) return;
+    this.ss.update({ ...settings, lighting: lighting });
   }
+
+  async changeHistorySize(size: number) {
+    const settings = this.settings;
+    if (!settings) return;
+    this.ss.update({ ...settings, historySize: size });
+  }
+
+  ngOnDestroy(): void {}
 }
